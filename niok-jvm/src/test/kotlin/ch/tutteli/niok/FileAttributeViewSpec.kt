87f87f18f21482161cccb84f127c463d43e93d3f
Robert Stoll <rstoll@tutteli.ch>
2019-11-27 21:23:08 +0100

    fix breaking changes in tutteli-spek-extension due to update to 1.1.0

--- niok-jvm/src/test/kotlin/ch/tutteli/niok/FileAttributeViewSpec.kt
+++ niok-jvm/src/test/kotlin/ch/tutteli/niok/FileAttributeViewSpec.kt
@@ -4,15 +4,14 @@ import ch.tutteli.atrium.api.cc.en_GB.notToBeNull
 import ch.tutteli.atrium.api.cc.en_GB.returnValueOf
 import ch.tutteli.atrium.api.cc.en_GB.toBe
 import ch.tutteli.atrium.verbs.expect
-import ch.tutteli.spek.extensions.TempFolder
+import ch.tutteli.spek.extensions.memoizedTempFolder
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import java.nio.file.attribute.BasicFileAttributeView
 import java.nio.file.attribute.FileAttributeView
 
 object FileAttributeViewSpec: Spek({
-    val tempFolder = TempFolder.perTest()
-    registerListener(tempFolder)
+    val tempFolder by memoizedTempFolder()
 
     describe("Path.getFileAttributeView") {
         it("returns an existing view") {
