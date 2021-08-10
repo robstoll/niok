f68ca91d6b1a2b8de7cfdeb254e8f596389b1f14
Robert Stoll <rstoll@tutteli.ch>
2021-08-10 13:07:21 +0200

    still use targetCompatibility 1.8 for java

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,11 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
     }
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
