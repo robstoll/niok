8b44afdc810b9b14e9e6a2afbbbcfb1290126d19
Robert Stoll <rstoll@tutteli.ch>
2021-08-10 12:51:06 +0200

    still generate jdk6 byte code for 1.4.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,10 @@ repositories {
     mavenCentral()
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "1.6"
+    }
 }
 
 dependencies {
